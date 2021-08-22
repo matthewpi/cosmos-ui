255431af5418c85a2eb5d4ea802544bdeefb7042
Matthew Penner <me@matthewp.io>
2021-08-22 15:19:43 -0600

    types: fix 'any' issue with styled.<> syntax

--- stitches.config.ts
+++ stitches.config.ts
@@ -2,5 +2,5 @@ import { createStitches, CSS as StitchesCSS } from '@stitches/react';
 
 const stitches = createStitches({});
 
-export const { css, globalCss, keyframes, styled, theme, getCssText } = stitches;
+export const { css, config, globalCss, keyframes, styled, theme, getCssText } = stitches;
 export type CSS = StitchesCSS<typeof stitches>;
--- types/twin.d.ts
+++ types/twin.d.ts
@@ -1,26 +1,85 @@
-import styledImport, { css as cssImport } from '@stitches/react';
+import { css as cssImport, CSSProperties } from '@stitches/react';
+import type * as StyledComponent from '@stitches/react/types/styled-component';
+import type * as Util from '@stitches/react/types/util';
+
+import { config, CSS } from '../stitches.config';
 
 import 'twin.macro';
 
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface DefaultTheme {}
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+type AnyIfEmpty<T extends object> = keyof T extends never ? any : T;
+
+interface CSSObject extends CSSProperties {
+	[key: string]: CSSObject | string | number | undefined;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 type CSSProp<T = AnyIfEmpty<DefaultTheme>> = CSSObject | string;
 
-declare module 'react' {
-	interface HTMLAttributes<T> extends DOMAttributes<T> {
-		css?: CSSProp;
-		tw?: string;
+type Stitches<Type = string> = <
+	Composers extends (
+		| string
+		| React.ExoticComponent<any>
+		| React.JSXElementConstructor<any>
+		| Util.Function
+		| { [name: string]: unknown }
+	)[],
+>(
+	...composers: {
+		[K in keyof Composers]: Composers[K] extends
+			| string
+			| React.ExoticComponent<any>
+			| React.JSXElementConstructor<any>
+			| Util.Function
+			? Composers[K]
+			: CSS & {
+					variants?: {
+						[Name in string]: {
+							[Pair in number | string]: CSS;
+						};
+					};
+					compoundVariants?: (('variants' extends keyof Composers[K]
+						? {
+								[Name in keyof Composers[K]['variants']]?:
+									| Util.Widen<keyof Composers[K]['variants'][Name]>
+									| Util.String;
+						  } &
+								Util.WideObject
+						: Util.WideObject) & {
+						css: CSS;
+					})[];
+					defaultVariants?: 'variants' extends keyof Composers[K]
+						? {
+								[Name in keyof Composers[K]['variants']]?:
+									| Util.Widen<keyof Composers[K]['variants'][Name]>
+									| Util.String;
+						  }
+						: Util.WideObject;
+			  } & {
+						[K2 in keyof Composers[K]]: K2 extends 'compoundVariants' | 'defaultVariants' | 'variants'
+							? unknown
+							: K2 extends keyof CSS
+							? CSS[K2]
+							: unknown;
+					};
 	}
+) => StyledComponent.StyledComponent<Type, StyledComponent.StyledComponentProps<Composers>, typeof config.media, CSS>;
 
-	interface SVGProps<T> extends SVGProps<T> {
+type StyledTags = {
+	[Element in keyof JSX.IntrinsicElements]: Stitches<Element>;
+};
+
+declare module 'react' {
+	interface Attributes {
 		css?: CSSProp;
 		tw?: string;
 	}
 }
 
-type StyledTags = {
-	[Tag in keyof JSX.IntrinsicElements]: CreateStyledComponent<JSX.IntrinsicElements[Tag]>;
-};
-
 declare module 'twin.macro' {
 	const css: typeof cssImport;
-	const styled: typeof styledImport | typeof StyledTags;
+	const styled: StyledTags;
 }
