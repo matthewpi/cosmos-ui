959bbfc8773a2b6b66c189e1fb5835bd3c731e15
Matthew Penner <me@matthewp.io>
2021-08-25 13:19:32 -0600

    convert to monorepo

--- packages/cosmos-ui/vite.config.ts
+++ packages/cosmos-ui/vite.config.ts
@@ -1,4 +1,4 @@
-import preact from '@preact/preset-vite';
+import reactRefresh from '@vitejs/plugin-react-refresh';
 import path from 'path';
 import { defineConfig } from 'vite';
 import macrosPlugin from 'vite-plugin-babel-macros';
@@ -12,22 +12,19 @@ export default defineConfig({
 		sourcemap: true,
 	},
 
-	plugins: [
-		preact(),
-		macrosPlugin(),
-	],
+	plugins: [reactRefresh(), macrosPlugin()],
 
 	resolve: {
 		alias: {
-			'react': 'preact/compat',
-			'react-dom/test-utils': 'preact/test-utils',
-			'react-dom': 'preact/compat',
-			'react/jsx-runtime': 'preact/jsx-runtime',
 			'~': path.resolve(__dirname, 'src'),
 		},
 	},
 
 	define: {
 		'process.env': {},
+	},
+
+	esbuild: {
+		jsxInject: "import React from 'react';",
 	},
 });
