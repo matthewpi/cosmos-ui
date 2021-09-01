fa2cd471e799b4f3b5c33b460b9267516e01ae6a
Matthew Penner <me@matthewp.io>
2021-09-01 10:12:19 -0600

    yarn: upgrade dependencies

--- packages/cosmos-ui/stitches.config.ts
+++ packages/cosmos-ui/stitches.config.ts
@@ -1,6 +1,6 @@
-import { createStitches, CSS as StitchesCSS } from '@stitches/react';
+import { createStitches } from '@stitches/react';
 
-const stitches = createStitches({});
+const stitches = createStitches();
 
 export const { css, config, globalCss, keyframes, styled, theme, getCssText } = stitches;
-export type CSS = StitchesCSS<typeof stitches>;
+export type { CSS } from '@stitches/react/types/css-util';
