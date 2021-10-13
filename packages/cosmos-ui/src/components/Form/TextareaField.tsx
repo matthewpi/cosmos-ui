affe4097b43838e4dfc9de778ea236de89d1c3e8
Matthew Penner <me@matthewp.io>
2021-10-13 10:30:52 -0600

    yarn: switch back to node-modules linker

--- packages/cosmos-ui/src/components/Form/TextareaField.tsx
+++ packages/cosmos-ui/src/components/Form/TextareaField.tsx
@@ -4,17 +4,19 @@ import { forwardRef } from 'react';
 import tw, { styled } from 'twin.macro';
 
 import { FieldWrapper } from '~/components/Form/Field';
+import { Label } from '~/components/Form/Label';
 
 const TextareaInput = styled.textarea({
-	...tw`block w-full px-3 py-2 border rounded-md shadow-sm appearance-none focus:outline-none focus:ring-pink-500 focus:border-pink-500 sm:text-sm`,
+	...tw`block w-full px-3 py-2 bg-transparent border rounded-md shadow-sm appearance-none focus:outline-none sm:text-sm`,
+
 	variants: {
 		hasError: {
 			true: {
-				...tw`text-red-900 border-red-300`,
+				...tw`text-red-500 border-red-400 focus:border-red-500 focus:ring-red-500`,
 				'&::placeholder': tw`text-red-300`,
 			},
 			false: {
-				...tw`border-grey-300`,
+				...tw`text-white border-grey-800 focus:border-pink-500 focus:ring-0`,
 				'&::placeholder': tw`text-grey-400`,
 			},
 		},
@@ -31,31 +33,33 @@ interface fieldProps {
 type FieldProps = fieldProps & Omit<React.TextareaHTMLAttributes<HTMLTextAreaElement>, 'name'>;
 
 export const TextareaField = forwardRef<HTMLTextAreaElement, FieldProps>(function Field(
-	{ id, name, label, showLabel, description, ...props },
+	{ id, name, label, showLabel: _showLabel, description, ...props },
 	ref,
 ) {
+	const showLabel = _showLabel === undefined ? true : _showLabel;
+
 	return (
 		<FField innerRef={ref} name={name}>
 			{({ field, meta: { error, touched } }: FFieldProps) => (
 				<div>
 					{label && (
-						<label htmlFor={id} css={showLabel ? tw`block text-sm font-medium text-grey-700` : tw`sr-only`}>
+						<Label htmlFor={id} show={showLabel}>
 							{label}
-						</label>
+						</Label>
 					)}
 
 					<FieldWrapper hasLabel={showLabel}>
-						<TextareaInput id={id} {...field} {...props} hasError={error !== undefined && touched} />
+						<TextareaInput {...field} {...props} id={id} hasError={error !== undefined && touched} />
 
 						{error !== undefined && touched && (
 							<div css={tw`absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none`}>
-								<ExclamationCircleIcon css={tw`w-5 h-5 text-red-500`} aria-hidden="true" />
+								<ExclamationCircleIcon css={tw`w-5 h-5 text-red-400`} aria-hidden="true" />
 							</div>
 						)}
 					</FieldWrapper>
 
 					{error !== undefined && touched ? (
-						<p id={id + '-error'} css={tw`mt-2 text-sm text-red-600`}>
+						<p id={id + '-error'} css={tw`mt-2 text-sm text-red-400`}>
 							{error.charAt(0).toUpperCase() + error.slice(1)}
 						</p>
 					) : description ? (
